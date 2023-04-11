e2faf200df25786440064074a3cb3122b3e6bf5c
velasquezmarcelo <velasquezmarcelo96@gmail.com>
2023-04-11 18:05:57 -0300

    finished homework

--- 03-sequelize/homework/middlewares/character.js
+++ 03-sequelize/homework/middlewares/character.js
@@ -2,4 +2,23 @@ const { Router } = require('express');
 const { Op, Character, Role } = require('../db');
 const router = Router();
 
+router.post('/', async (req, res) => {
+    const {code, name, age, race, hp, mana} = req.body; 
+    if(![code, name, hp, mana].every(Boolean))  
+        return res.status(404).send("Falta enviar datos obligatorios");
+    try {
+        const newCharacter = await Character.create({
+            code,
+            name,
+            age,
+            race,
+            hp,
+            mana,
+        });
+        res.status(201).json(newCharacter);
+    } catch (error) {
+        res.status(404).send( "Error en alguno de los datos provistos");
+    }    
+});
+
 module.exports = router;
